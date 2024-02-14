29bd06aa4edf80f6c364aa36f167cdacb410c6c7
norieau <a.arijogja@gmail.com>
2024-02-15 00:07:17 +0700

    setting up a bot app

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+require('dotenv').config()
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
